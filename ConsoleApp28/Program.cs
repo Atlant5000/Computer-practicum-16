897ceebdf5226580aa70732292d965d868916737
Максим <maximmmd13@gmail.com>
2025-01-26 12:32:49 +0200

    Task 4

---
+++ ConsoleApp28/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    /// <summary>
+    ///  Вывести на экран прямоугольник вида □, обрамленный символами (например, 16 на 16) и пустой внутри.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+
+            for (i = 0; i < 8; i++)
+
+            {
+
+                for (j = 1; j < 1 + i; j++)
+
+                Console.Write(" ");
+
+                Console.WriteLine("#");
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
